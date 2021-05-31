d5957882a3fe12b44ab553afb2836d9f5e654b0f
Falcon Rising <chinnidiwakar5@gmail.com>
2021-05-31 17:37:36 +0530

    Update moduletest.py

--- moduletest.py
+++ moduletest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+#inport the module as a whole
 import math
+#or import the specific func out of a module
 from math import sqrt
+#use the func from the whole module
 print (math.factorial(5))
+#use the func from specific import
 print (sqrt(100))
