b00423e080fe9d75c3e3ce6dd4c640ffc4d98da0
Falcon Rising <chinnidiwakar5@gmail.com>
2021-05-31 19:13:06 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+a="CHINNI"
+b=10
+c=13.6
+d=('a','b','c','d')
+e={'a','b','c','d'}
+f=['a','b','c','d']
+g=True
+h={"name" : "John", "age" : 36}
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
