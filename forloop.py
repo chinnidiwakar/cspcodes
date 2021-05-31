c456c327a0039952da0cf18d8a85e961611c5880
Falcon Rising <chinnidiwakar5@gmail.com>
2021-05-31 17:24:49 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+a=int(input("How many times you want to print: "))
+for x in range(0, a):
+    print("We're on time %d" % (x))
+for x in range(0,a):
+    print("192.168.0.%d"%(x))
