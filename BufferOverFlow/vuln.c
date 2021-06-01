1d66634df76f8d06cd9536587948a74249439578
Falcon Rising <chinnidiwakar5@gmail.com>
2021-06-01 18:18:14 +0530

    Update vuln.c

--- BufferOverFlow/vuln.c
+++ BufferOverFlow/vuln.c
@@ -4,21 +4,13 @@
 void vuln_func(char *input);
 
 int main(int argc, char *argv[])
-
 {
-
 if(argc>1)
-
 vuln_func(argv[1]);
-
 }
 
 void vuln_func(char *input)
-
 {
-
 char buffer[256];
-
 strcpy(buffer, input);
-
 }
