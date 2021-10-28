142f62ff72034bd0067d219417d7af31b31727c0
Falcon Rising <chinnidiwakar5@gmail.com>
2021-10-28 22:08:20 +0530

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -16,3 +16,48 @@ for port in range(1, 1000):
         print('Pord %d: OPEN' %(port))
     #close the tcp connection.
     sock.close()
+
+    
+    
+    
+#!/usr/bin/env python3
+
+#install the below components
+#python3 -m pip install --upgrade pip
+#pip3 install validators
+
+#import necessary modules
+import ipaddress
+import validators
+from socket import *
+
+#take input
+ipdom=input("Please Enter Your Target IP: ")
+
+try:
+    #validate the ip
+    ip=ipaddress.ip_address(ipdom)
+    print ("\nPlease wait while ports are being scanned\n")
+    #Scan for ports in loop
+    for port in range(1,65535):
+        s=socket(AF_INET,SOCK_STREAM)
+        conn=s.connect_ex((str(ip),port))
+        if(conn==0):
+            print('Port %d: OPEN'%(port))
+        s.close()
+except ValueError:
+    #validate the domain
+    if(validators.domain(ipdom)==True):
+        print ("\nPlease wait while ports are being scanned\n")
+        #Extract IP of the domain
+        ip=gethostbyname(ipdom)
+        #Scan for ports in loop
+        for port in range(1,65535):
+            s=socket(AF_INET,SOCK_STREAM)
+            conn=s.connect_ex((str(ip),port))
+            if(conn==0):
+                print('Port %d: OPEN'%(port))
+            s.close() 
+    else:
+        #Print Error.
+        print ("Please enter a valid IP/Domain")
