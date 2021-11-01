9821685645926132cf5b12fcca1b075dd6f545d0
Falcon Rising <chinnidiwakar5@gmail.com>
2021-11-01 20:00:58 +0530

    Update networkscanner.py

--- networkscanner.py
+++ networkscanner.py
@@ -1,24 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+#take input from user
+range=input("Please Enter Network Range: ")
+#print the info fromt he dictionary
+print ("Printing Available Devices In the Target Network: \n")
 #import necessary functions from scapy
 from scapy.all import ARP, Ether, srp
-#take input from user
-nw=input("Please Enter Network Range: ")
 #make a arp packet with ARP function and user data
-arp=ARP(pdst=nw)
+arp=ARP(pdst=range)
 #define a broadcast ip
-brdcst= Ether(dst="ff:ff:ff:ff:ff:ff")
+bdcast=Ether(dst="ff:ff:ff:ff:ff:ff")
 #build the packet
-packet= brdcst/arp
+packet= bdcast/arp
 #make a blank dictionary to save the output
-clients = []
+targets= []
 #prepare the packet with all the necessary info using srp
-result = srp(packet, timeout=3, verbose=0)[0]
-
+gotip=srp(packet, timeout=3, verbose=0)[0]
 #loop and send packets and get data and add that to your dictionary
-for sent, received in result:
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
-
-#print the info fromt he dictionary
-print ("Printing Available Devices In the Target Network: ")
-for device in clients:
-    print(device['ip'],device['mac'])
+for sent, received in gotip:
+    print(received.psrc, received.hwsrc)
