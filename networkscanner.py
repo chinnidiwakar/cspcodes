613da82651769124e4e544fad697f250e448c9d8
Falcon Rising <chinnidiwakar5@gmail.com>
2022-01-10 14:09:55 +0530

    Update networkscanner.py

--- networkscanner.py
+++ networkscanner.py
@@ -18,3 +18,18 @@ gotip=srp(packet, timeout=3, verbose=0)[0]
 #loop and send packets and get data and add that to your dictionary
 for sent, received in gotip:
     print(received.psrc, received.hwsrc)
+
+    
+    
+#from scapy.all import *
+
+#interface = "eth0"
+#ip_range = "10.10.X.X/24"
+#broadcastMac = "ff:ff:ff:ff:ff:ff"
+
+#packet = Ether(dst=broadcastMac)/ARP(pdst = ip_range) 
+
+#ans, unans = srp(packet, timeout =2, iface=interface, inter=0.1)
+
+#for send,receive in ans:
+#        print (receive.sprintf(r"%Ether.src% - %ARP.psrc%"))  
