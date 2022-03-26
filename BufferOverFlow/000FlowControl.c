482151bd3ba42905f65156e3f0e727f74ecc3448
Falcon Rising <chinnidiwakar5@gmail.com>
2022-03-26 10:04:32 +0530

    Create 000FlowControl.c

---
+++ BufferOverFlow/000FlowControl.c
@@ -0,0 +1,16 @@
+//turn off ASLR
+//echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+//turn off DEP
+//gcc -fno-stack-protector -z execstack program.c -o program
+#include <stdio.h>
+#include <string.h>
+void secret(){
+printf(“You have accessed the secret function!!\n”);
+}
+int main(int argc, char *argv[])
+{
+char buf[512];
+strcpy(buf, argv[1]);
+printf(“%s”,buf);
+return(0);
+}
