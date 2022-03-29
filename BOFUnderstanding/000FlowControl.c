81f2626785b2488ce39eb59aa23e0c7fc994baaa
Falcon Rising <chinnidiwakar5@gmail.com>
2022-03-29 18:26:37 +0530

    add

--- BOFUnderstanding/000FlowControl.c
+++ BOFUnderstanding/000FlowControl.c
@@ -5,12 +5,12 @@
 #include <stdio.h>
 #include <string.h>
 void secret(){
-printf(“You have accessed the secret function!!\n”);
+printf("You have accessed the secret function!!\n");
 }
 int main(int argc, char *argv[])
 {
 char buf[512];
 strcpy(buf, argv[1]);
-printf(“%s”,buf);
+printf("%s",buf);
 return(0);
 }
---
+++ networkscanner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+figlet -ctf big  "WELCOME TO MY NETWORK SCANNER"
+figlet -f slant "by 5H4D0W3Y7E_5U1D"
+echo "    "
+
+echo "(Please Wait Scanner is starting)"
+echo "     "
+read -p "Please Enter Your IP Range: " ip
+echo "   "
+figlet -ctf slant "P L E A S E  W A I T  S C A N N I N G  I S  I N  P R O G R E S S"
+for i in {1..255}
+do
+	ping $ip$i -c 1 |grep "bytes from" | cut -d " " -f4 &
+done
