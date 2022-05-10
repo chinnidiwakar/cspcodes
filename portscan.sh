bca286af4c60af3dcfe7ee572abd3a35870a02a0
Falcon Rising <chinnidiwakar5@gmail.com>
2022-05-10 19:50:05 +0530

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo  "Welcome to port scanner\n"
 read -p "Please enter IP adderss" input
+#in ZSH read "?prompt: " input
 echo "\nScanning the ports Please wait!!!!!"
 nc -nvz $input 1-65534 > /tmp/file.txt 2>&1
 cat /tmp/file.txt |grep "UNKNOWN"|cut -d " " -f3,5|tac
