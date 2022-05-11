636a79321a3688b97ef406c5001e99bd6ee71844
Falcon Rising <chinnidiwakar5@gmail.com>
2022-05-11 21:20:07 +0530

    Update 008networkscanner.py

--- 008networkscanner.py
+++ 008networkscanner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+"""#!/usr/bin/env python3
 #take input from user
 range=input("Please Enter Network Range: ")
 #print the info fromt he dictionary
@@ -17,7 +17,7 @@ targets= []
 gotip=srp(packet, timeout=3, verbose=0)[0]
 #loop and send packets and get data and add that to your dictionary
 for sent, received in gotip:
-    print(received.psrc, received.hwsrc)
+    print(received.psrc, received.hwsrc)"""
 
     
     
@@ -26,10 +26,19 @@ for sent, received in gotip:
 interface = "eth0"
 ip_range = "10.10.X.X/24"
 broadcastMac = "ff:ff:ff:ff:ff:ff"
-
 packet = Ether(dst=broadcastMac)/ARP(pdst = ip_range) 
-
 ans, unans = srp(packet, timeout =2, iface=interface, inter=0.1)
-
 for send,receive in ans:
         print (receive.sprintf(r"%Ether.src% - %ARP.psrc%"))  """
+
+
+
+#!/usr/bin/env python3
+from scapy.all import ARP, Ether, srp
+nw=input("Please enter your network range: ")
+arp=ARP(pdst=nw)
+bdcast=Ether(dst="ff:ff:ff:ff:ff:ff")
+packet=bdcast/arp
+scan=srp(packet, timeout=3, verbose=0)[0]
+for sent, received in scan:
+    print(received.psrc, received.hwsrc)
