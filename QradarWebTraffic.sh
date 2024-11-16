46bf328d94bdbac7676997fb8902517fb0b0e835
Falcon Rising <chinnidiwakar5@gmail.com>
2024-11-16 17:32:02 +0530

    Create QradarWebTraffic.sh

---
+++ QradarWebTraffic.sh
@@ -0,0 +1,10 @@
+apt update && apt install curl -y
+curl -I ifconfig.me
+curl -I http://www.altoromutual.com/
+curl -I testphp.vulnweb.com
+curl -I testasp.vulnweb.com
+curl -I testhtml5.vulnweb.com
+curl -I http://www.svrtravels.com/
+curl -I http://certifiedhacker.com/
+curl -I http://juggyboy.com/
+curl -I http://hackthissite.com/
