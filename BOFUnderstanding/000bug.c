74ab90f35051a3c447ab796137a29326c9e46d4a
Falcon Rising <chinnidiwakar5@gmail.com>
2024-12-22 14:44:03 +0530

    Update 000bug.c

--- BOFUnderstanding/000bug.c
+++ BOFUnderstanding/000bug.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<string.h>
 
-int main(int argc, char *argv[])
-{
-if(argc>1)
-vuln_func(argv[1]);
-else
-printf("Not Enough Arguments, Plz Try Again\n");
-}
-
 void vuln_func(char *input)
 {
 char buffer[256];
 strcpy(buffer, input);
 printf(input);
 printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+if(argc>1)
+vuln_func(argv[1]);
+else
+printf("Not Enough Arguments, Plz Try Again\n");
 }
